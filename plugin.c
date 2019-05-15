55113f0b90c09bbc61409d29aa0d5e1e3ff85e19
whileX <yx.develop@gmail.com>
2019-05-15 17:35:01 -0500

    minimum

--- plugin.c
+++ plugin.c
@@ -45,6 +45,7 @@ typedef struct suggested_export {
 }suggested_export;
 volatile __emem __export uint32_t global_semaphores[STATE_TABLE_SIZE + 1] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 __shared __export __addr40 __emem bucket_list state_hashtable[STATE_TABLE_SIZE + 1];
+__shared __export __addr40 __emem uint32_t index;
 void semaphore_down(volatile __declspec(mem addr40) void * addr) {
     unsigned int addr_hi, addr_lo;
     __declspec(read_write_reg) int xfer;
@@ -103,10 +104,13 @@ int pif_plugin_state_update(EXTRACTED_HEADERS_T *headers,
     uint32_t root;
     uint32_t swap;
     uint32_t reverse;
-    uint32_t heap_arr_index[BUCKET_SIZE];
+//    uint32_t heap_arr_index[BUCKET_SIZE];
     __xread uint32_t heap_size_r;
-    __xread uint32_t heap_arr_rw[BUCKET_SIZE];
-    __xrw uint32_t suggestion_rw[BUCKET_SIZE + 1];
+//    __xread uint32_t heap_arr_r[BUCKET_SIZE];
+//    __xread uint32_t suggestion_r[BUCKET_SIZE + 1];
+//    __xrw uint32_t exportIndex = 0;
+//    uint32_t minimum;
+//    __xread uint32_t rowValue;
 
     ipv4 = pif_plugin_hdr_get_ipv4(headers);
 
@@ -134,12 +138,18 @@ int pif_plugin_state_update(EXTRACTED_HEADERS_T *headers,
     update_hash_value = hash_me_crc32((void *)update_hash_key,sizeof(update_hash_key), 1);
 
     update_hash_value &= (STATE_TABLE_SIZE);
-    for (i = 0; i < BUCKET_SIZE; i++){
-        heap_arr_index[i] = i;
-    }
+//    for (i = 0; i < BUCKET_SIZE; i++){
+//        heap_arr_index[i] = i;
+//    }
     semaphore_down(&global_semaphores[update_hash_value]);
+    
+//    mem_read_atomic(heap_arr_r, state_hashtable[update_hash_value].row, sizeof(heap_arr_r)); /* TODO: Read whole bunch at a time */
     for (i = 0; i < BUCKET_SIZE; i++) {
         mem_read_atomic(hash_key_r, state_hashtable[update_hash_value].entry[i].key, sizeof(hash_key_r)); /* TODO: Read whole bunch at a time */
+//        mem_read_atomic(&rowValue, &state_hashtable[update_hash_value].row[i], sizeof(rowValue)); /* TODO: Read whole bunch at a time */
+//        if(heap_arr_r[i] >= heap_arr_r[exportIndex]){
+//            exportIndex = i;//Find exportIndex
+//        }
         if (hash_key_r[0] == update_hash_key[0] &&
             hash_key_r[1] == update_hash_key[1] &&
             hash_key_r[2] == update_hash_key[2] ) { /* Hit */
@@ -155,101 +165,53 @@ int pif_plugin_state_update(EXTRACTED_HEADERS_T *headers,
             }
             break;
         }
-        else if (hash_key_r[0] == 0) {
+        else if (hash_key_r[0] == 0) {//insert or replace existing one
+//            mem_read_atomic(&heap_size_r, &state_hashtable[update_hash_value].heap_size, sizeof(heap_size_r));
             b_info = &state_hashtable[update_hash_value];
+//            if(heap_size_r < 5){//insert in entry i and update the counter, keep at most 5 entry
+//                mem_incr32(&b_info->heap_size);
             key_addr =(__addr40 uint32_t *) state_hashtable[update_hash_value].entry[i].key;
-            
             tmp_b_info = 1;
             mem_write_atomic(&tmp_b_info, &b_info->row[i], sizeof(tmp_b_info));
             mem_write_atomic(key_val_rw,(__addr40 void *)key_addr, sizeof(key_val_rw));
-            heap_size_rw = i + 1;//potential heap size - 1
-            mem_write_atomic(&heap_size_rw,&b_info->heap_size, sizeof(heap_size_rw));
+//            }
+//            else{// we need to replace one, which will be exported
             
-            mem_read_atomic(&heap_size_r, &state_hashtable[update_hash_value].heap_size, sizeof(heap_size_r));
-            suggestion_rw[BUCKET_SIZE] = heap_size_r;
-            //heapify
-            mem_read_atomic(heap_arr_rw, state_hashtable[update_hash_value].row, sizeof(heap_arr_rw));
-            if(heap_size_r >= 2){
-                for (j = 0; j <= heap_size_r / 2 - 1; j++){
-                    reverse = heap_size_r / 2 - 1 - j;
-                    while(1){
-                        largest = reverse; // Initialize largest as root
-                        
-                        // If left child is larger than root
-                        if (2*reverse + 1 < heap_size_r && heap_arr_rw[heap_arr_index[2*reverse + 1]] > heap_arr_rw[heap_arr_index[largest]])
-                            largest = 2*reverse + 1;
-                        
-                        // If right child is larger than largest so far
-                        if (2*reverse + 2 < heap_size_r && heap_arr_rw[heap_arr_index[2*reverse + 2]] > heap_arr_rw[heap_arr_index[largest]])
-                            largest = 2*reverse + 2;
-                        
-                        // If largest is not root
-                        if (largest != reverse)
-                        {
-                            swap = heap_arr_index[largest];
-                            heap_arr_index[largest] = heap_arr_index[reverse];
-                            heap_arr_index[reverse] = swap;
-                        }else{
-                            break;
-                        }
-                        reverse = largest;
-                    }
-                }
-                
-                // One by one extract an element from heap
-                for (j = 0; j <= heap_size_r - 1; j++)
-                {
-                    reverse = heap_size_r - 1 - j;
-                    // Move current root to end
-                    swap = heap_arr_index[0];
-                    heap_arr_index[0] = heap_arr_index[reverse];
-                    heap_arr_index[reverse] = swap;
-                    
-                    root = 0;
-                    while(1){
-                        largest = root; // Initialize largest as root
-                        
-                        // If left child is larger than root
-                        if (2*root + 1 < reverse && heap_arr_rw[heap_arr_index[2*root + 1]] > heap_arr_rw[heap_arr_index[largest]])
-                            largest = 2*root + 1;
-                        
-                        // If right child is larger than largest so far
-                        if (2*root + 2 < reverse && heap_arr_rw[heap_arr_index[2*root + 2]] > heap_arr_rw[heap_arr_index[largest]])
-                            largest = 2*root + 2;
-                        
-                        // If largest is not root
-                        if (largest != root)
-                        {
-                            swap = heap_arr_index[largest];
-                            heap_arr_index[largest] = heap_arr_index[root];
-                            heap_arr_index[root] = swap;
-                        }else{
-                            break;
-                        }
-                        root = largest;
-                    }
-                    
-                }
-                for (j = 0;j < heap_size_r; j++){
-                    suggestion_rw[heap_size_r - j - 1] = heap_arr_index[j];
-                }
-                for (j = heap_size_r;j < BUCKET_SIZE; j++){
-                    suggestion_rw[j] = heap_arr_index[j];
-                }
-                mem_write_atomic(suggestion_rw,state_hashtable[update_hash_value].suggestion, sizeof(suggestion_rw));
-                
-            }
+                //export heap_arr_index[heap_size_r - 1] and replace:
+                //export
+                //replace
+//                key_addr =(__addr40 uint32_t *) state_hashtable[update_hash_value].entry[exportIndex].key;
+//                tmp_b_info = 1;
+//                mem_write_atomic(&tmp_b_info, &b_info->row[exportIndex], sizeof(tmp_b_info));
+//                mem_write_atomic(key_val_rw,(__addr40 void *)key_addr, sizeof(key_val_rw));
+//                for (j = 0;j < heap_size_r; j++){
+//                    suggestion_rw[heap_size_r - j - 1] = heap_arr_index[j];
+//                }
+//                for (j = heap_size_r;j < BUCKET_SIZE; j++){
+//                    suggestion_rw[j] = heap_arr_index[j];
+//                }
+//                mem_write_atomic(suggestion_rw,state_hashtable[update_hash_value].suggestion, sizeof(suggestion_rw));
+
+//            }
             
             break;
         }
+//        if (i == BUCKET_SIZE - 1){
+//            mem_write_atomic(&exportIndex, &index, sizeof(exportIndex));
+//            key_addr =(__addr40 uint32_t *) state_hashtable[update_hash_value].entry[exportIndex].key;
+//            tmp_b_info = 1;
+//            mem_write_atomic(&tmp_b_info, &b_info->row[exportIndex], sizeof(tmp_b_info));
+//            mem_write_atomic(key_val_rw,(__addr40 void *)key_addr, sizeof(key_val_rw));
+//        }
+
     }
     semaphore_up(&global_semaphores[update_hash_value]);
     
     
-    /* If bucket full, drop */
-
-    if (i == BUCKET_SIZE)
-	return PIF_PLUGIN_RETURN_FORWARD;
+//    /* If bucket full, drop */
+//
+//    if (i == BUCKET_SIZE)
+//    return PIF_PLUGIN_RETURN_FORWARD;
 
     return PIF_PLUGIN_RETURN_FORWARD;
 
